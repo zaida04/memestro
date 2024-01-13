6ed55c732430b3db02a871c142039e9885e803b8
Zaid Arshad (Nico) <nico.03727+github@gmail.com>
2024-01-13 18:36:11 -0500

    feat: even better navbar

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,63 +2,60 @@ import { Feather } from "@expo/vector-icons";
 import { Tabs } from "expo-router/tabs";
 import React from "react";
 
-const hiddenTabBarRoutes = ["details"];
+const tabs = [
+	{
+		name: "index",
+		label: "Home",
+		icon: "home",
+	},
+	{
+		name: "profile",
+		label: "Profile",
+		icon: "user",
+	},
+	{
+		name: "create-post",
+		label: "Create Post",
+		icon: "plus",
+	},
+	{
+		name: "search",
+		label: "Search",
+		icon: "search",
+	},
+	{
+		name: "settings",
+		label: "Settings",
+		icon: "settings",
+	},
+]
 const noop = () => null;
 
 function hideTab(options: any) {
 	return {
-		tabBarButton: hiddenTabBarRoutes.includes(options.route.name) ? noop : undefined,
+		tabBarButton: !tabs.some(x => x.name === options.route.name)
+			? noop
+			: undefined,
 	};
 }
 
 export default function Layout() {
 	return (
-		<Tabs screenOptions={hideTab} initialRouteName="index">
-			<Tabs.Screen
-				name="index"
-				options={{
-					tabBarLabel: "Home",
-					tabBarIcon(props) {
-						return <Feather name="home" {...props} />;
-					},
-				}}
-			/>
-			<Tabs.Screen
-				name="profile"
-				options={{
-					tabBarLabel: "Profile",
-					tabBarIcon(props) {
-						return <Feather name="user" {...props} />;
-					},
-				}}
-			/>
-			<Tabs.Screen
-				name="create-post"
-				options={{
-					tabBarLabel: "Create Post",
-					tabBarIcon(props) {
-						return <Feather name="plus" {...props} />;
-					},
-				}}
-			/>
-			<Tabs.Screen
-				name="search"
-				options={{
-					tabBarLabel: "Search",
-					tabBarIcon(props) {
-						return <Feather name="search" {...props} />;
-					},
-				}}
-			/>
-			<Tabs.Screen
-				name="settings"
-				options={{
-					tabBarLabel: "Settings",
-					tabBarIcon(props) {
-						return <Feather name="settings" {...props} />;
-					},
-				}}
-			/>
+		<Tabs
+			screenOptions={hideTab}
+			initialRouteName="index">
+			{tabs.map((tab) => (
+				<Tabs.Screen
+					key={tab.name}
+					name={tab.name}
+					options={{
+						tabBarLabel: tab.label,
+						tabBarIcon(props) {
+							return <Feather name={tab.icon as any} {...props} />;
+						},
+					}}
+				/>
+			))}
 		</Tabs>
 	);
 }
