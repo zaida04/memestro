18a93903a2dc4f07c0218f9b3cecd669dc7de157
Zaid Arshad (Nico) <nico.03727+github@gmail.com>
2024-01-15 03:15:51 -0500

    feat: more scaffolding

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,6 +5,11 @@ import homeRouter from "./routes/home";
 const app = new Elysia();
 app.use(homeRouter);
 
+app.get("/", ({ set }) => {
+	set.status = 301;
+	set.redirect = "/v1/";
+});
+
 app.listen(5000, () => {
 	logger.info(`🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`);
 });
--- api/src/routes/home.ts
+++ api/src/routes/home.ts
@@ -1,7 +1,13 @@
 import { Elysia } from "elysia";
-
+const pkg = await Bun.file("package.json").text();
+const parsedPkg = JSON.parse(pkg);
 const homeRouter = new Elysia();
 
-homeRouter.get("/", () => "Hello Elysia");
+homeRouter.get("/v1", () => {
+	return {
+		message: "Welcome to the Memestro API!",
+		version: parsedPkg.version,
+	};
+});
 
 export default homeRouter;
